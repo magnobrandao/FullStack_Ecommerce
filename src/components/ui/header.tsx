431511bc66a34f8d23624560b024b4199da34375
Magno Brandao <magnoboc@gmail.com>
2023-10-22 23:38:41 -0300

    feat: add sheet component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,15 +1,40 @@
-import { MenuIcon, ShoppingCartIcon } from "lucide-react";
+import { HomeIcon, ListOrderedIcon, LogInIcon, MenuIcon, PercentCircleIcon, PercentIcon, ShoppingCartIcon } from "lucide-react";
 import { Button } from "./button";
-import { Card, CardContent } from "./card";
+import { Card } from "./card";
+import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
 
 const Header = () => {
     return (
         <Card className="flex justify-between p-[1.875rem] items-center">
+            <Sheet>
+                <SheetTrigger asChild>
+                    <Button size="icon" variant="outline">
+                        <MenuIcon />
+                    </Button>
+                </SheetTrigger>
 
-            <Button size="icon" variant="outline">
-                <MenuIcon />
-            </Button>
-
+                <SheetContent side="left">
+                    <SheetHeader className="text-left text-lg font-semibold">Menu</SheetHeader>
+                    <div className="mt-2 flex flex-col gap-3">
+                        <Button variant="outline" className="w-full justify-start text-lg gap-2">
+                            <LogInIcon size={16} />
+                            Fazer Login
+                        </Button>
+                        <Button variant="outline" className="w-full justify-start text-lg gap-2">
+                            <HomeIcon size={16} />
+                            Início
+                        </Button>
+                        <Button variant="outline" className="w-full justify-start text-lg gap-2">
+                            <PercentIcon size={16} />
+                            Ofertas
+                        </Button>
+                        <Button variant="outline" className="w-full justify-start text-lg gap-2">
+                            <ListOrderedIcon size={16} />
+                            Catálogo
+                        </Button>
+                    </div>
+                </SheetContent>
+            </Sheet>
             <h1 className="text-lg font-semibold">
                 <span className="text-primary">MAGNO</span> Store</h1>
 
